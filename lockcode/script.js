390ddf625aef6e193b995039648f04d06ed8f8b0
VIVEK S <138084941+theviveks@users.noreply.github.com>
2024-10-04 21:49:36 +0530

    Add files via upload

---
+++ lockcode/script.js
@@ -0,0 +1,26 @@
+const passbox=document.getElementById("password");
+const length = 16;
+const uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const num = "0123456789";
+const symbol = "~!@#$%^&*()_[]{}?";
+ 
+const allchar = uppercase+lowercase+num+symbol;
+
+function gpass(){
+    let password = "";
+    password+= uppercase[Math.floor(Math.random()*uppercase.length)];
+    password+= lowercase[Math.floor(Math.random()*lowercase.length)];
+    password+= num[Math.floor(Math.random()*num.length)];
+    password+= symbol[Math.floor(Math.random()*symbol.length)];
+
+    while(length> password.length){
+        password+= allchar[Math.floor(Math.random()*allchar.length)];
+    }
+    passbox.value = password;
+} 
+
+function copypass(){
+    passbox.select();
+    document.execCommand("copy")
+}
